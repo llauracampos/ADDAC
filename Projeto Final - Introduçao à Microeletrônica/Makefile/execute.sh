df0cfb6a89b04256e2b357eef73b81b01a10d107
Laura Campos <41303661+llauracampos@users.noreply.github.com>
2021-11-18 18:47:31 -0300

    Add files via upload

---
+++ Projeto Final - Introduçao à Microeletrônica/Makefile/execute.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+make inicial
+make genpat1
+make vasy
+make boom
+make proof
+make boog
+make genpat2
+make loon
+make ocp
+make nero
+make cougar
+make lvx
+make genlib
+make mudar
+make ring
+make cougar
+make x2y
